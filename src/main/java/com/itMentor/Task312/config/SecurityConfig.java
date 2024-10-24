4c670c6277ccfe1a031dab32bafd2ae08a5b2703
semmi1986 <samasiuk1986@gmail.com>
2024-10-24 16:28:16 +0300

    add solution 3.1.2

--- src/main/java/com/itMentor/Task312/config/SecurityConfig.java
+++ src/main/java/com/itMentor/Task312/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.itMentor.Task312.config;
 
-import com.itMentor.Task312.service.UserAuthDetailServiceImpl;
+import com.itMentor.Task312.service.Impl.UserAuthDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,43 +17,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    private final UserAuthDetailServiceImpl userAuthDetailService;
+
     @Autowired
-    private UserAuthDetailServiceImpl userAuthDetailService;
+    public SecurityConfig(UserAuthDetailServiceImpl userAuthDetailService) {
+        this.userAuthDetailService = userAuthDetailService;
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers("/home", "/register/**").permitAll();
-                    registry.requestMatchers("/admin/**", "/home").hasRole("ADMIN");
-                    registry.requestMatchers("/user/**").hasRole("USER");
-                    registry.anyRequest().authenticated();
-                })
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .authorizeHttpRequests(registry ->
+                        registry.requestMatchers("/register/**").permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/userInfo/**").hasAnyRole("USER", "ADMIN")
+                )
+                .formLogin(form -> form
+                        .successHandler(new LoginSuccessHandler())
+                        .permitAll()
+                )
                 .build();
-    }
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//
-//        UserDetails normalUser = User.builder()
-//                .username("easy")
-//                .password("$2a$12$ytCh.AKSv6LJopBJJiyF5eIgkmftc67PhsW2lGMQ03F5CIZHVzT.2") //123
-//                .roles("USER")
-//                .build();
-//        UserDetails adminUser = User.builder()
-//                .username("admin")
-//                .password("$2a$12$2rPBxYTazBEegCxr/bs2jONvBi1oy9LxPHNBn3mgT7udwapi/ZkQG") //456
-//                .roles("ADMIN", "USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(normalUser, adminUser);
-//    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return userAuthDetailService;
     }
 
     @Bean
--- src/main/java/com/itMentor/Task312/controller/UsersResController.java
+++ src/main/java/com/itMentor/Task312/controller/UsersResController.java
@@ -1,7 +1,7 @@
 package com.itMentor.Task312.controller;
 
-import com.itMentor.Task312.model.UserAuth;
-import com.itMentor.Task312.repositories.UserAuthRepository;
+import com.itMentor.Task312.model.User;
+import com.itMentor.Task312.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UsersController {
+public class UsersResController {
 
     @Autowired
-    private UserAuthRepository userAuthRepository;
+    private UserRepository userAuthRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register/user")
-    public UserAuth createUser(@RequestBody UserAuth user) {
+    public User createUser(@RequestBody User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userAuthRepository.save(user);
     }
