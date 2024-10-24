9c59a0cba1fbb8f6c8813cfff859aa371eba99b9
semmi1986 <samasiuk1986@gmail.com>
2024-10-24 22:55:15 +0300

    ini

--- src/main/java/com/itMentor/Task312/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.itMentor.Task312.repositories;
-
-import com.itMentor.Task312.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/itMentor/Task312/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.itMentor.Task312.service;
-
-import com.itMentor.Task312.model.Role;
-
-import java.util.List;
-
-public interface RoleService {
-    List<Role> findAll();
-}
